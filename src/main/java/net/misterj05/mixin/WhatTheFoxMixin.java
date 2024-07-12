bbaada3d3ebbf4f94b517d4148ec8db3d62816e1
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-07-12 07:33:58 -0400

    debug stuff

--- src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
+++ src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
@@ -1,16 +1,29 @@
 package net.misterj05.mixin;
 
+import net.minecraft.entity.passive.FoxEntity;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+import javax.swing.plaf.synth.SynthStyle;
+
+@Mixin(FoxEntity.JumpChasingGoal.class)
 public class WhatTheFoxMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-		System.out.println("loadWorld has been called.");
+	@Inject(at = @At("TAIL"), method = "canStop", cancellable = true)
+	private void canStop(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(true);
+	}
+
+	@Inject(at = @At("HEAD"), method = "start")
+	private void Start(CallbackInfo info) {
+		System.out.println("Fox has started jump.");
+	}
+
+	@Inject(at = @At("HEAD"), method = "stop")
+	private void Stop(CallbackInfo info) {
+		System.out.println("Fox has stopped jump.");
 	}
 }
