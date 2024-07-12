ab558096c1e4425fd3643a2d7dfba02906b53831
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-07-12 18:43:08 -0400

    cleanup imports

--- src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
+++ src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
@@ -1,8 +1,6 @@
 package net.misterj05.mixin;
 
-import net.minecraft.entity.ai.goal.DiveJumpingGoal;
 import net.minecraft.entity.passive.FoxEntity;
-import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,13 +8,11 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import javax.swing.plaf.synth.SynthStyle;
-
 @Mixin(FoxEntity.JumpChasingGoal.class)
 public abstract class WhatTheFoxMixin {
 
-	@Inject(at = @At("RETURN"), method = "shouldContinue", cancellable = true)
+	@Inject(at = @At("HEAD"), method = "shouldContinue", cancellable = true)
 	private void shouldContinue(CallbackInfoReturnable<Boolean> cir) {
-		cir.setReturnValue(false);
+
 	}
 }
