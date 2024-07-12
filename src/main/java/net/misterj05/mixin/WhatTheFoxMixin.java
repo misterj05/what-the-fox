99ad1a01b3daad3fefdfbf914f8037cba8517a69
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-07-12 07:56:02 -0400

    getting somewhere

--- src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
+++ src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
@@ -1,8 +1,10 @@
 package net.misterj05.mixin;
 
+import net.minecraft.entity.ai.goal.DiveJumpingGoal;
 import net.minecraft.entity.passive.FoxEntity;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,10 +13,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import javax.swing.plaf.synth.SynthStyle;
 
 @Mixin(FoxEntity.JumpChasingGoal.class)
-public class WhatTheFoxMixin {
-	@Inject(at = @At("TAIL"), method = "canStop", cancellable = true)
-	private void canStop(CallbackInfoReturnable<Boolean> cir) {
-		cir.setReturnValue(true);
+public abstract class WhatTheFoxMixin {
+
+	@Inject(at = @At("RETURN"), method = "shouldContinue", cancellable = true)
+	private void shouldContinue(CallbackInfoReturnable<Boolean> cir) {
+		// FoxEntity.JumpChasingGoal thisObject = (FoxEntity.JumpChasingGoal)(Object)this;
+		System.out.println("Pre Inject: shouldContinue = " + cir.getReturnValue());
+		cir.setReturnValue(false);
+		System.out.println("Post Inject: shouldContinue = " + cir.getReturnValue());
 	}
 
 	@Inject(at = @At("HEAD"), method = "start")
