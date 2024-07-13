db4a35c5d9739f41ff86af6186b2ece6a5777f29
misterj05 <44005965+misterj05@users.noreply.github.com>
2024-07-12 23:47:43 -0400

    final polish for 0.1

--- src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
+++ src/main/java/net/misterj05/mixin/WhatTheFoxMixin.java
@@ -1,8 +1,10 @@
 package net.misterj05.mixin;
 
 import net.minecraft.entity.passive.FoxEntity;
+import java.util.Timer;
+import java.util.TimerTask;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -10,9 +12,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(FoxEntity.JumpChasingGoal.class)
 public abstract class WhatTheFoxMixin {
+	@Unique
+	private Timer timer = new Timer();
+	@Unique
+	private boolean taskScheduled = false;
+	@Unique
+	private boolean stopJump = false;
 
-	@Inject(at = @At("HEAD"), method = "shouldContinue", cancellable = true)
-	private void shouldContinue(CallbackInfoReturnable<Boolean> cir) {
+	@Inject(at = @At("RETURN"), method = "shouldContinue", cancellable = true)
+	private void canStop(CallbackInfoReturnable<Boolean> cir) {
+		if (stopJump && !taskScheduled) {
+			stopJump = false;
+			cir.setReturnValue(false);
+		}
+	}
 
+	@Inject(at = @At("HEAD"), method = "start")
+	private void start(CallbackInfo info) {
+		if (!taskScheduled) {
+			taskScheduled = true;
+			TimerTask task = new TimerTask() {
+				@Override
+				public void run() {
+					taskScheduled = false;
+					stopJump = true;
+				}
+			};
+			timer.schedule(task, 1200);
+		}
 	}
 }
